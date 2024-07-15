4b3a81e77fea6aa893656ba5d322c2e0188ee991
JHC <2280055436@qq.com>
2024-07-16 02:36:10 +0800

    23ok

--- k210_projects/k210_uart_test/TxRx_RxTx.py
+++ k210_projects/k210_uart_test/TxRx_RxTx.py
@@ -1,13 +1,13 @@
 
 import sensor, image, time, lcd, sys
 
-lcd.init()                          
-lcd.clear(lcd.WHITE)             
-sensor.reset()                      
-sensor.set_pixformat(sensor.RGB565) 
-sensor.set_framesize(sensor.QVGA)   
-sensor.skip_frames(time = 2000)    
-clock = time.clock()               
+lcd.init()
+lcd.clear(lcd.WHITE)
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time = 2000)
+clock = time.clock()
 from fpioa_manager import fm
 from machine import UART
 
@@ -26,23 +26,23 @@ try:
     while True:
 
         img = sensor.snapshot()         # Take a picture and return the image.
-        
+
         if time.ticks_ms() - last_time > 500:
             last_time = time.ticks_ms()
             uart.write(write_bytes)
-       
-        
+
+
         if uart.any():
             read_data = uart.read()
-            print("recv = ", read_data) 
-            
-          
+            print("recv = ", read_data.decode('utf-8'))
+
+
         img.draw_string(2,200, read_data, color=(0, 176, 80), scale=2)
-        lcd.display(img)              
-       
+        lcd.display(img)
+
 
 except:
     pass
-    
+
 uart.deinit()
 del uart
